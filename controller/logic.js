a47155e548bd9e6f68b729d2f685c78f550c7f1a
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2022-12-03 00:33:13 +0545

    Error in Error Handler

--- controller/logic.js
+++ controller/logic.js
@@ -3,6 +3,8 @@ const tasks = require("../models/tasks");
 //asyncWrapper helps us in refactoring the code by avoiding repetation of try catch blocks in controllers.
 const asyncWrapper = require("../middleware/async");
 
+const { createCustomError } = require("../errors/custom-error");
+
 const getAllTask = asyncWrapper(async (req, res) => {
   const task = await tasks.find({});
   res.status(200).json({ task });
@@ -30,9 +32,7 @@ const getTask = asyncWrapper(async (req, res) => {
   //
   //edi task bhetiyena bhanye task null hunxa tesaile yo tala ko function null napathauna rakheko ho
   if (!tasksMaster) {
-    return res
-      .status(404)
-      .json({ msg: `Sorry, No Task of id: ${taskID} found!` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   //
   //task bhetiyema hamile tyo task response pathauxau
@@ -47,9 +47,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   //tasks.findByIdAndDelete le chai tyo id khojera teslai database bata delete gardaxa
   const tasksMaster = await tasks.findByIdAndDelete({ _id: taskID });
   if (!tasksMaster) {
-    return res.status(404).json({
-      msg: `Sorry, No Task of id: ${taskID} found in the database!`,
-    });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({
     msg: `Task with ID of ${taskID} has been deleted sucessfully from the database.`,
@@ -69,9 +67,7 @@ const updateTask = asyncWrapper(async (req, res) => {
 
   if (!tasksMaster) {
     console.log("K bhayo");
-    return res.status(404).json({
-      msg: `Sorry, No Task of id: ${taskID} found in the database!`,
-    });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json(tasksMaster);
 });
